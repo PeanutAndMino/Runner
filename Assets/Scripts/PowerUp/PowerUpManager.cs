19ff21552b8f3673f34b0c89851d1d1da135aad0
Andrés Vieira <anvieiravazquez@gmail.com>
2019-01-12 17:42:06 +0100

    Adds Power Up manager

--- Assets/Scripts/PowerUp/PowerUpManager.cs
+++ Assets/Scripts/PowerUp/PowerUpManager.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class PowerUpManager : MonoBehaviour
 {
+    public List<PowerUpSO> powerUpList = new List<PowerUpSO>();
+    private int currentPowerUpIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (powerUpList.Count < 1)
+        {
+            Debug.LogError("The PowerUpManager component on the character needs to have at least 1 power up");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public void nextPowerUp()
+    {
+        if (currentPowerUpIndex < powerUpList.Count - 1)
+        {
+            currentPowerUpIndex++;
+        }
+        else
+        {
+            currentPowerUpIndex = 0;
+        }
+    }
+
+    public void previousPowerUp()
+    {
+        if (currentPowerUpIndex > 0)
+        {
+            currentPowerUpIndex++;
+        }
+        else
+        {
+            currentPowerUpIndex = powerUpList.Count - 1;
+        }
+    }
+
+    public PowerUpSO getCurrentPowerUpScriptableObject()
+    {
+        return powerUpList[currentPowerUpIndex];
+    }
+
+    public PowerUp currentPowerUpType()
+    {
+        return this.getCurrentPowerUpScriptableObject().powerUpType;
     }
 }
