bfd772e37e0ce03e947c83bfc7b1da33a8bc6b2d
Andrés Vieira <anvieiravazquez@gmail.com>
2019-01-12 17:18:49 +0100

    Adds power ups scriptable objects

---
+++ Assets/Scripts/PowerUp/PowerUp.cs
@@ -0,0 +1,8 @@
+﻿public enum PowerUp
+{
+    JUMP = 0,
+    DOUBLE_JUMP = 1,
+    DIG = 2,
+    GLIDE = 3,
+    DASH = 4
+};
---
+++ Assets/Scripts/ScriptableObjects/PowerUpSO.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "PowerUp", menuName = "Power Ups/Power Up", order = 1)]
+public class PowerUpSO : ScriptableObject
+{
+    public string powerUpName = "A Power Up";
+    public PowerUp powerUpType = PowerUp.JUMP;
+}
